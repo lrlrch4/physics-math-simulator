cebed3d18c5af8e9481ffbb04067196d45a05a96
lrlrch4 <luis.larosa.c@uni.pe>
2024-05-13 22:06:55 -0500

    DataCurve object created

---
+++ object-classes/coordinateObjects/DataCurve.js
@@ -0,0 +1,71 @@
+class DataCurve { 
+    constructor(props){ 
+        this.input = props.input || [0, 1, 2, 3];
+        this.output = props.output || [2, 5, 7, 1];
+
+        this.color = props.color || '#0af';
+        this.opacity = props.opacity || 1;
+        this.lineWidth = props.lineWidth || 5; 
+
+        this.layer = 0;
+
+        this.simulation = props.simulation || (()=>{
+            'simulation added'
+        });
+        this.animation = props.animation || (()=>{
+            'simulation added'
+        })
+    }
+
+    //Graphics props
+    draw(){
+        const layer = canvas.getContext('2d');
+        layer.save();
+        layer.strokeStyle = this.color; 
+        layer.lineWidth = this.lineWidth;
+        layer.globalAlpha = this.opacity;
+        layer.beginPath();
+        for(let s = 0; s < this.input.length; s++){  
+            const coordinates = {x: this.input[s], y: this.output[s]}          
+            const pixels = xy.coordinatesToPixels(coordinates);
+            layer.lineTo(
+                pixels.x,
+                pixels.y
+            )
+        }
+        layer.stroke();
+        layer.restore();
+
+        if(this.isClicked){ 
+            new CoordinatePoint({
+                pos: this.pointClicked, 
+                label: `(${this.pointClicked.x.toFixed(2)}, ${this.pointClicked.y.toFixed(2)})`,
+                radius: 10,
+                color: this.color, 
+                labelSize: 50
+            }).draw()
+        }
+    }
+
+    rotate(){}
+
+    applyMatrixTransformation(){}
+
+    //Interaction props
+    onMouseDown(event){}
+
+    onMouseMove(event){}
+
+    onMouseUp(){}
+
+    //Animation props
+    animate(){
+        this.animation();
+    }
+
+    simulate(){
+        this.simulation();
+    }
+    showTrace(){
+    }
+}
--- scenes/whiteScene.js
+++ scenes/whiteScene.js
@@ -1,7 +1,7 @@
 backgroundColor = '#000000';
-unit = 300;
+unit = 250;
 camera = {
-    x: .5*canvas.width, y: .5*canvas.height
+    x: 200, y: 1000
 }
 xy.origin = {
     x: camera.x, 
@@ -11,25 +11,21 @@ xy.grid = true;
 xy.horizontalAxis = true;
 xy.verticalAxis = true;
 
-const curve = new ParametricCurve({
-    mathFunction: ((s) => ({x: Math.cos(s), y: Math.sin(s)})),
-    range: {start: 0, end: 2*Math.PI}
-})
+const inputData = Array.from(
+    {length: 200}, 
+    (element, index) => .05*index
+);
 
-const point = new CoordinatePoint({ 
-    pos: {x: 1, y: 0}, 
-    onMouseMoveAddition: ((event) => {
-        const cMouse = xy.pixelsToCoordinates({
-            x: resolutionFactor*event.x, 
-            y: resolutionFactor*event.y
-        })
-        console.log(cMouse)
-        const s0 = Math.atan2(cMouse.y, cMouse.x);
+const outputData = Array.from({
+    length: inputData.length}, 
+    (element, index) => Math.random()
+);
 
-        point.pos = curve.mathFunction(s0)
-    })
-})
+const dataCurve = new DataCurve({
+    input: inputData,
+    output: outputData
+});
 
-
-drawObjects.push(curve, point);
-interactiveObjects.push(point);
+drawObjects.push(dataCurve);
+interactiveObjects.push();
+simulationObjects.push()
