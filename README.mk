8d16ee67e7dc6d25fda0e8e759ce1f192517cfc2
lrlrch4 <luis.larosa.c@uni.pe>
2024-05-16 20:52:53 -0500

    index modified

--- README.mk
+++ README.mk
@@ -2,9 +2,11 @@
 
 This is a math and physics engine and calculator for academic purposes. It is inspired on geogebra but with personalized features to be more suitable for more advance uses, especially in math and physics topics. It is created canvas with javascript and HTML. 
 
+You can create your own scene in the scenes files. To execute it: 
+node server.js 'myScene.js'
+
+
 Object general structure:
-
-
 class CoordinateObject { 
     constructor(props){ 
         
@@ -51,7 +53,7 @@ interactiveObjects = [];
 animatedObjects = [];
 simulationObjects = [];
 
-Code to start: 
+Code to the starting configuration: 
 backgroundColor = '#000000';
 unit = 280;
 camera = {
