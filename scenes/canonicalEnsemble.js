c666a0e1cd87d4374d02a3bf3fc7c7a437d43bf9
lrlrch4 <luis.larosa.c@uni.pe>
2025-10-04 01:57:57 -0500

    canonical ensemble updated

--- scenes/canonicalEnsemble.js
+++ scenes/canonicalEnsemble.js
@@ -1,8 +1,8 @@
 backgroundColor = '#000000'; //Background color of the simulation
 showSceneData = true; //Show scene frames, time, resolution, etc. 
-unit = 100; // pixels of a unit in coordinate system
+unit = 30; // pixels of a unit in coordinate system
 camera = {
-    x: .5*canvas.width, y: 0.5*canvas.height
+    x: 0.05*canvas.width, y: 0.9*canvas.height
 } //Position of the window camera in window coordinates
 xy.origin = {
     x: camera.x, 
@@ -13,9 +13,12 @@ xy.horizontalAxis = true; //Show horizontal axis in scene
 xy.verticalAxis = true; //Show vertical axis in scene
 
 const n = 100;
-const temperature = 5.0;
+const temperature = 10;
+const scale = 1;
+const r = 10;
+const iterationsPerFrame = 10;
 
-
+let iterations = 0;
 let energies = new Array(n).fill(0);  // Inicialmente todos en estado fundamental (n=0)
 let m = 0;
 
@@ -40,20 +43,27 @@ const monteCarloStep = () => {
 
 const oscillators = Array.from({ length: n }, (_, index) => {
     return new CoordinatePoint({
-        pos: { x: index, y: energies[index] },
+        pos: { x: index + 1, y: energies[index] },
+        radius: r,
     });
 });
 
 const oscillatorsSorted = Array.from({ length: n }, (_, index) => {
     return new CoordinatePoint({
-        pos: { x: index, y: energies[index] },
-        color: '#f00'
+        pos: { x: index + 1, y: energies[index] },
+        color: '#f00', 
+        radius: r
     });
 });
 
 
+const mt = n / (Math.exp(1/temperature) - 1);
+const tEntropy = n*Math.log((n+mt)/n) + mt*Math.log((n+mt)/mt);
+const tChemPot = - Math.log((mt+n)/n)/Math.log((mt+n)/mt)
+const tCapacity = (mt/n)*(mt+n)*( Math.log((n+mt)/mt) )**2
 
-let iterations = 0;
+
+
 const canonicalData = new Text({
     text: [], 
     pos: {x: 400, y: 100}, 
@@ -61,27 +71,17 @@ const canonicalData = new Text({
     color: '#FFFFFF',
     animation: (() => {
         
-        const iterationsPerFrame = 10;
+        
         if(frame > 0) {            
             for (let i = 0; i < iterationsPerFrame; i++) {
                 monteCarloStep(); 
                 iterations++; 
             }     
         }
-
-        const mt = n / (Math.exp(1/temperature) - 1);
         
-
-        const tEntropy = n*Math.log((n+mt)/n) + mt*Math.log((n+mt)/mt);
         const sEntropy = n*Math.log((n+m )/n) + m *Math.log((n+m )/m);
-
-        const tChemPot = - Math.log((mt+n)/n)/Math.log((mt+n)/mt)
-        const sChemPot = - Math.log((m+n )/n)/Math.log((m +n)/m);   
-
-        const tCapacity = (m/n)*(m+n)*( Math.log((n+m)/m) )**2
-        const sCapacity = (mt/n)*(mt+n)*( Math.log((n+mt)/mt) )**2
-
-        
+        const sChemPot = - Math.log((m+n )/n)/Math.log((m +n)/m);           
+        const sCapacity = (m/n)*(m+n)*( Math.log((n+m)/m) )**2;        
 
         canonicalData.text = [
         `Iterations: ${iterations}`, 
@@ -90,18 +90,18 @@ const canonicalData = new Text({
         `(S. packets, T. packets) : (${m}, ${mt.toFixed(0)})`,         
         `(S. entropy, T. entropy) : (${sEntropy.toFixed(2)}, ${tEntropy.toFixed(2)})`,
         `(S. chem. pot., T. chem. pot.): (${sChemPot.toFixed(2)}, ${tChemPot.toFixed(2)})`, 
-        `(S. Capacity, T. Capacity): (${tCapacity.toFixed(2)}, ${sCapacity.toFixed(2)})`, 
+        `(S. Capacity, T. Capacity): (${sCapacity.toFixed(2)}, ${tCapacity.toFixed(2)})`, 
         ]
 
         oscillators.forEach((oscillator, index) => {
-            oscillator.pos.y = energies[index];
+            oscillator.pos.y = scale*energies[index];
         });       
 
         const sortedEnergies = [...energies].sort((a,b) => b-a)
         
 
         oscillatorsSorted.forEach((oscillator, index) => {
-            oscillator.pos.y = sortedEnergies[index];
+            oscillator.pos.y = scale*sortedEnergies[index];
         });       
 
     })
